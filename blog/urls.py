e5e5295eebf9a7a6ecae4fe358e03a842c27744f
Tega Rorobi <rorobitega@gmail.com>
2023-10-26 23:21:55 +0100

    Registered the created views in the blog app's urlconf.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import re_path
+from .views import PostsListView, PostCreateUpdateView
 
 app_name = 'blog'
+
 urlpatterns = [
-	
+	re_path('^list[-_]?(posts)?/?$', PostsListView.as_view(), name='posts-list'),
+	re_path('^create[-_]?(post)?/?$', PostCreateUpdateView.as_view(), name='post-create'),
+	re_path('^update[-_]?(post)?/(?P<id>\d+)/?$', PostCreateUpdateView.as_view(), name='post-update'),
 ]
