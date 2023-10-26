30fc48052976fe42f519081f7bcf4998839d5718
Tega Rorobi <rorobitega@gmail.com>
2023-10-26 19:44:17 +0100

    Registered models in the admin, configured the __str__ methods of the models.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post, Comment 
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	model = Post 
+	list_display = 'title', 'content', 'created_at', 'updated_at'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	model = Comment 
+	list_display = 'user', 'blog', '_content', 'created_at', 'updated_at'
+
+	@admin.display()
+	def _content(self, obj):
+		content = obj.content
+		return content if len(content) <= 20 else content[:40]+'...'
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from src.utils import shorten
 
 # Create your models here.
 class Post(models.Model):
@@ -10,14 +11,20 @@ class Post(models.Model):
 	created_at = models.DateTimeField(auto_now=True)
 	updated_at = models.DateTimeField(auto_now_add=True)
 
+	def __str__(self):
+		return self.title 
 
 
 class Comment(models.Model):
-	blog = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
 	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+	blog = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
 
 	content = models.TextField()
 
 	# timestamps
 	created_at = models.DateTimeField(auto_now=True)
 	updated_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		content = self.content
+		return content if len(content) <= 20 else content[:20]+'...'
