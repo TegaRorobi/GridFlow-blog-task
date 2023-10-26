4a5080094b2c784a1d3c6f4e5c72f68b99c39c84
Tega Rorobi <rorobitega@gmail.com>
2023-10-26 20:30:26 +0100

    Created forms for posts and comments

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms 
+from .models import Post
+
+class PostForm(forms.ModelForm):
+	class Meta:
+		model = Post 
+		fields = 'title', 'content'
+
+
+class CommentForm(forms.ModelForm):
+	class Meta:
+		model = Comment 
+		fields = 'content',
