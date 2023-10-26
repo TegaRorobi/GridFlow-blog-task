35e01719ac686879a234f892d6f39de44bf8dee5
Tega Rorobi <rorobitega@gmail.com>
2023-10-26 23:19:18 +0100

    Created two views; A view for listing posts, and another for creating and updating them.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
-from src.utils import shorten
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
 class Post(models.Model):
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import View
+from django.urls import reverse
+from django.contrib import messages
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
+
 
 # Create your views here.
+class PostsListView(View):
+	def get(self, request, *args, **kwargs):
+		posts = Post.objects.all()
+		return render(request, 'blog/post-list.html', {'posts':posts})
+
+
+
+class PostCreateUpdateView(View):
+	def get(self, request, *args, **kwargs):
+		create = 'id' not in kwargs
+		if create:
+			form = PostForm()
+			return render(request, 'blog/post-create_update.html', {'form':form, 'create':True})
+
+		post = get_object_or_404(Post, id=kwargs.get('id', 0))
+		form = PostForm(instance=post)
+		return render(request, 'blog/post-create_update.html', {'form':form, 'create':False})
+
+
+	def post(self, request, *args, **kwargs):
+		create = 'id' not in kwargs
+		if create:
+			form = PostForm(request.POST)
+		else:
+			post = get_object_or_404(Post, id=kwargs.get('id', 0))
+			form = PostForm(request.POST, instance=post)
+		
+		if form.is_valid:
+			form.save()
+			return redirect(reverse('blog:posts-list'))
+		messages.error('Invalid input.')
+		return self.get(request)
